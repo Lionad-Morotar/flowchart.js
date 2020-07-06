cc45e499817f5b09de134cd74221b9d78523ae60
Lionad <tangnad@qq.com>
2020-07-06 23:44:17 +0800

    chore: ...

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
   env: {
+    amd: true,
     browser: true,
     commonjs: true,
     es2020: true,
--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 export default {
   input: 'index.js',
-  plugins: [require('rollup-plugin-node-resolve')(), require('rollup-plugin-commonjs')()],
-  external: ['raphael'],
+  plugins: [require('rollup-plugin-commonjs')(), require('rollup-plugin-node-resolve')()],
+  external: ['raphael', /node_modules/],
   output: {
     name: 'flowchart',
     file: './release/flowchart.js',
